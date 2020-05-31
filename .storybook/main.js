3bb6a55d7ff9e6787c849df14b9a663b8c41a606
Kyle Fong <krfong@ucsc.edu>
2020-05-31 00:18:22 -0700

    enable scss: add css loaders and node-sass

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,11 @@
+const path = require("path")
 module.exports = {
-	stories: ["../src/**/*.stories.js"],
-};
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    })
+    return config
+  },
+  stories: ["../src/**/*.stories.js"],
+}
