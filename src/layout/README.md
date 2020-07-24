da3fb875615cf143e3bc25ef95efb619becd117b
Kyle Fong <krfong@ucsc.edu>
2020-07-23 20:43:44 -0700

    Add grid and flexbox support for creating a container

---
+++ src/layout/README.md
@@ -0,0 +1,24 @@
+# Layout
+
+Swish Layout is a responsive layout that adapts to screen size and orientation.
+
+## Design
+
+Swish uses CSS Flexbox Layout in order to implement alignment, sizing, and distribution of space.
+(Thought/Question: should we define a coarse-grained and fine-grained API and create a table for methods available for both?)
+
+## Usage
+
+## Contributing
+
+If you're interested in what makes Swish Layout work, you'll find the source code in the `src` folder. If you aren't familiar with CSS Flexbox, you can gain an understanding after reading this article [A Complete Guide to Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/).
+
+## Acknowledgements
+
+Swish Layout is heavily influenced from the following Design Systems:
+
+- Atlassian
+- Flexport
+- Material
+- Carbon
+- [React-Flexbox-Grid](https://github.com/roylee0704/react-flexbox-grid)
--- src/layout/layout-demo.tsx
+++ src/layout/layout-demo.tsx
@@ -3,32 +3,39 @@ import "./swish-grid.scss";
 import "./demo.css";
 export const LayoutDemo: React.SFC = () => (
   <div>
-    <div className="swish-grid demo-flex-grid">
-      <div className="demo-flex-row">1</div>
-      <div className="demo-flex-row">2</div>
-      <div className="demo-flex-row">3</div>
-      <div className="demo-flex-row">4</div>
-      <div className="demo-flex-row">5</div>
-      <div className="demo-flex-row">6</div>
-      <div className="demo-flex-row">7</div>
-      <div className="demo-flex-row">8</div>
-      <div className="demo-flex-row">9</div>
-      <div className="demo-flex-row">10</div>
-      <div className="demo-flex-row">11</div>
-      <div className="demo-flex-row">12</div>
-    </div>
-    <div className="demo-grid">
-      <div className="demo-grid-row" id="span-4">
-        Span 4 columns, yeah, 4 cols.
+    <div className="swish-grid">
+      <div className="swish-grid-inner">
+        <div className="swish-grid-cell">1</div>
+        <div className="swish-grid-cell">2</div>
+        <div className="swish-grid-cell">3</div>
+        <div className="swish-grid-cell">
+          4 ok so then what happens when this is a really huge box, do we
+          overflow?
+        </div>
+        <div className="swish-grid-cell">5</div>
+        <div className="swish-grid-cell">6</div>
+        <div className="swish-grid-cell">7</div>
+        <div className="swish-grid-cell">8</div>
+        <div className="swish-grid-cell">9</div>
+        <div className="swish-grid-cell">10</div>
+        <div className="swish-grid-cell">11</div>
+        <div className="swish-grid-cell">12</div>
       </div>
-      <div className="demo-grid-row" id="placement-8">
-        Placed on 8
+    </div>
+    <div className="swish-grid">
+      <div className="swish-grid-inner">
+        <div className="sdc-grid-row" id="span-4">
+          Span 4 columns, yeah, 4 cols.
+        </div>
+        <div className="sdc-grid-row" id="placement-8">
+          Placed on 8
+        </div>
       </div>
     </div>
   </div>
 );
 
-// <div className="sdc-grid demo-grid">
+// <div className="sdc-grid sdc-grid">
 //   <div className="sdc-grid-cell sdc-grid-cell--span-6 demo-grid-cell" />
 //   <div className="sdc-grid-cell sdc-grid-cell--span-2 demo-grid-cell" />
 //   <div className="sdc-grid-cell sdc-grid-cell--span-5 demo-grid-cell" />
