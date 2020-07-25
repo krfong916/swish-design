33cfb5d2071579a5a6a9df1bf1b8bca14b902fa9
Kyle Fong <krfong@ucsc.edu>
2020-07-25 16:25:14 -0700

    Add flex and grid layout. WIP: flex-order, nesting grid

--- src/layout/layout-demo.tsx
+++ src/layout/layout-demo.tsx
@@ -3,32 +3,43 @@ import "./swish-grid.scss";
 import "./demo.css";
 export const LayoutDemo: React.SFC = () => (
   <div>
-    <div className="swish-grid">
-      <div className="swish-grid-inner">
-        <div className="swish-grid-cell">1</div>
-        <div className="swish-grid-cell">2</div>
-        <div className="swish-grid-cell">3</div>
-        <div className="swish-grid-cell">
-          4 ok so then what happens when this is a really huge box, do we
-          overflow?
+    <div className="sdc-grid">
+      <div className="sdc-grid-inner">
+        <div className="sdc-grid-col sdc-grid-col--span-4">1</div>
+        <div className="sdc-grid-col">2</div>
+        <div className="sdc-grid-col">3</div>
+        <div className="sdc-grid-col">
+          <span>
+            4 ok so then what happens when this is a really huge box, do we
+            overflow?
+          </span>
         </div>
-        <div className="swish-grid-cell">5</div>
-        <div className="swish-grid-cell">6</div>
-        <div className="swish-grid-cell">7</div>
-        <div className="swish-grid-cell">8</div>
-        <div className="swish-grid-cell">9</div>
-        <div className="swish-grid-cell">10</div>
-        <div className="swish-grid-cell">11</div>
-        <div className="swish-grid-cell">12</div>
+        <div className="sdc-grid-col">
+          <h1>
+            let's see how far we can take this and if the max-width wil overlfow
+            what will happen idk idkjfhfldjgdfjgbdfsjgbdfsjlgbdfskjg
+            dfkjgnfdsjkgneakjgheraihjerai;oheraio;haerg
+            ergeraiovnergnegiernag;eklrf wafbweiobfweof weoif weoib
+          </h1>
+        </div>
+        <div className="sdc-grid-col">6</div>
+        <div className="sdc-grid-col">7</div>
+        <div className="sdc-grid-col">8</div>
+        <div className="sdc-grid-col">9</div>
+        <div className="sdc-grid-col">10</div>
+        <div className="sdc-grid-col">11</div>
+        <div className="sdc-grid-col">12</div>
       </div>
     </div>
-    <div className="swish-grid">
-      <div className="swish-grid-inner">
-        <div className="sdc-grid-row" id="span-4">
-          Span 4 columns, yeah, 4 cols.
-        </div>
-        <div className="sdc-grid-row" id="placement-8">
-          Placed on 8
+    <div className="sdc-grid">
+      <div className="sdc-grid-inner">
+        <div className="sdc-grid-col">Span 4 columns, yeah, 4 cols.</div>
+        <div className="sdc-grid-col">
+          <div className="sdc-grid">
+            <div className="sdc-grid-inner">
+              <div className="sdc-grid-col">nested</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
