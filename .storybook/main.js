ecf547da44ef643d7248866bf047f8a18c71b870
Kyle Fong <krfong@ucsc.edu>
2021-02-11 11:41:22 -0800

    Port to typescript

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,40 @@
 const path = require("path");
-const swishConfig = require("../webpack.config");
 module.exports = {
   stories: ["../src/**/*.stories.tsx", "../src/**/*.stories.js"],
   addons: [
     "@storybook/addon-a11y",
     "@storybook/addon-actions",
-    "@storybook/addon-docs",
     "@storybook/addon-knobs",
     "@storybook/addon-storysource",
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+      },
+    },
   ],
   webpackFinal: async config => {
-    config.module.rules.push(...swishConfig.module.rules);
+    config.module.rules.push(
+      {
+        test: /\.(ts|tsx)$/,
+        include: path.resolve(__dirname, "../src"),
+        use: [
+          require.resolve("ts-loader"),
+          {
+            loader: require.resolve("react-docgen-typescript-loader"),
+            options: {
+              // We need our stories to display types from outside each indiv. story
+              tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
+            },
+          },
+        ],
+      },
+      {
+        test: /\.scss$/,
+        include: path.resolve(__dirname, "../src"),
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    );
     config.resolve.extensions.push(".ts", ".tsx");
     return config;
   },
