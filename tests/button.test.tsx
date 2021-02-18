babb5e6420a97e1f47d8866bf0ef18c269c51b07
Kyle Fong <krfong@ucsc.edu>
2021-02-18 00:39:44 -0800

    enable release on test branch, fix tests. WIP: ship only component package

--- tests/button.test.tsx
+++ tests/button.test.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { render, screen, cleanup } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import {Button} from "../src/components/";
+import { Button } from "../src/components";
 
 afterEach(cleanup);
 
