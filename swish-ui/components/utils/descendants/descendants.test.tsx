0421b483f5e193426b3271b4a8cf19bc58d0e657
Kyle Fong <krfong@ucsc.edu>
2021-07-19 22:51:17 -0700

    include declarations in our npm package,add pin-input test

--- swish-ui/components/utils/descendants/descendants.test.tsx
+++ swish-ui/components/utils/descendants/descendants.test.tsx
@@ -9,65 +9,65 @@ please be mindful that the top-level component is declaring Provider context, an
 */
 
 import * as React from "react";
-// import { Button } from "../../button";
-// import { render, screen, fireEvent } from "@testing-library/react";
-// import userEvent from "@testing-library/user-event";
-// import {
-//   DescendantProvider,
-//   useDescendants,
-//   useDescendant,
-//   Descendant,
-//   createDescendantContext,
-// } from "./index";
-// import { createNamedContext } from "../createNamedContext";
+import { Button } from "../../button";
+import { render, screen, fireEvent } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import {
+  DescendantProvider,
+  useDescendants,
+  useDescendant,
+  Descendant,
+  createDescendantContext,
+} from "./index";
+import { createNamedContext } from "../createNamedContext";
 
 // intersection type: combine descendant type with properties of an accordion descendant
-// type AccordionDescendant = Descendant & {
-//   focused?: boolean;
-//   disabled?: boolean;
-// };
+type AccordionDescendant = Descendant & {
+  focused?: boolean;
+  disabled?: boolean;
+};
 
-// const AccordionDescendantContext = createDescendantContext<AccordionDescendant>(
-//   "AccordionDescendantContext",
-// );
+const AccordionDescendantContext = createDescendantContext<AccordionDescendant>(
+  "AccordionDescendantContext",
+);
 
-// type AccordionItem = {
-//   prop?: boolean;
-// };
-// const initAccordionItem = {} as AccordionItem;
-// const AccordionItemContext = createNamedContext<AccordionItem>(
-//   "AccordionItemContext",
-//   initAccordionItem,
-// );
+type AccordionItem = {
+  prop?: boolean;
+};
+const initAccordionItem = {} as AccordionItem;
+const AccordionItemContext = createNamedContext<AccordionItem>(
+  "AccordionItemContext",
+  initAccordionItem,
+);
 
-// function Accordion({ children }: { children: React.ReactNode }) {
-//   const autoFocus: boolean = true;
-//   const [descendants, setDescendants] = useDescendants<AccordionDescendant>();
+function Accordion({ children }: { children: React.ReactNode }) {
+  const autoFocus: boolean = true;
+  const [descendants, setDescendants] = useDescendants<AccordionDescendant>();
 
-//   return (
-//     <DescendantProvider
-//       setDescendants={setDescendants}
-//       context={AccordionDescendantContext}
-//       descendants={descendants}
-//     >
-//       <AccordionItemContext.Provider value={null}>
-//         {children}
-//       </AccordionItemContext.Provider>
-//     </DescendantProvider>
-//   );
-// }
+  return (
+    <DescendantProvider
+      setDescendants={setDescendants}
+      context={AccordionDescendantContext}
+      descendants={descendants}
+    >
+      <AccordionItemContext.Provider value={null}>
+        {children}
+      </AccordionItemContext.Provider>
+    </DescendantProvider>
+  );
+}
 
-// function AccordionItem({ children }: { children: React.ReactNode }) {
-//   return <div>{children}</div>;
-// }
+function AccordionItem({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
 
-// function AccordionButton({ children }: { children: React.ReactNode }) {
-//   return <button>{children}</button>;
-// }
+function AccordionButton({ children }: { children: React.ReactNode }) {
+  return <button>{children}</button>;
+}
 
-// function AccordionPanel({ children }: { children: React.ReactNode }) {
-//   return <div>{children}</div>;
-// }
+function AccordionPanel({ children }: { children: React.ReactNode }) {
+  return <div>{children}</div>;
+}
 
 describe("Descendants Context API", () => {
   /** 
@@ -80,27 +80,27 @@ describe("Descendants Context API", () => {
   focus on the next accordion item
   */
   test("focuses on the next element", () => {
-    //   const { container, getByText } = render(
-    //     <Accordion>
-    //       <AccordionItem>
-    //         <h2>
-    //           <AccordionButton>First section</AccordionButton>
-    //         </h2>
-    //         <AccordionPanel>Panel 1</AccordionPanel>
-    //       </AccordionItem>
-    //       <AccordionItem>
-    //         <h2>
-    //           <AccordionButton>Second section</AccordionButton>
-    //         </h2>
-    //         <AccordionPanel>Panel 2</AccordionPanel>
-    //       </AccordionItem>
-    //     </Accordion>,
-    //   );
-    //   const first = getByText("First section");
-    //   const second = getByText("Second section");
-    //   first.focus();
-    //   expect(first).toHaveFocus();
-    //   userEvent.tab();
-    //   expect(second).toHaveFocus();
+    const { container, getByText } = render(
+      <Accordion>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>First section</AccordionButton>
+          </h2>
+          <AccordionPanel>Panel 1</AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <h2>
+            <AccordionButton>Second section</AccordionButton>
+          </h2>
+          <AccordionPanel>Panel 2</AccordionPanel>
+        </AccordionItem>
+      </Accordion>,
+    );
+    const first = getByText("First section");
+    const second = getByText("Second section");
+    first.focus();
+    expect(first).toHaveFocus();
+    userEvent.tab();
+    expect(second).toHaveFocus();
   });
 });
